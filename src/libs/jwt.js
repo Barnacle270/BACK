541d08dd059fbc005b93f4dedff253ea65d95ea5
Jhostym <you@example.com>
2024-07-04 14:21:49 -0500

    sadsad

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -3,7 +3,11 @@ import jwt from "jsonwebtoken";
 
 export async function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+    jwt.sign(payload, TOKEN_SECRET, { 
+      expiresIn: "1d",
+      sameSite: "none",
+      secure: true,
+    }, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
