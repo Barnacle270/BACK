b390b475fac5709e9ed942e7003381a1901daef4
Jhostym <you@example.com>
2024-07-04 14:24:26 -0500

    asdasd

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -4,9 +4,7 @@ import jwt from "jsonwebtoken";
 export async function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
     jwt.sign(payload, TOKEN_SECRET, { 
-      expiresIn: "1d",
-      sameSite: "none",
-      secure: true,
+      expiresIn: "1d" 
     }, (err, token) => {
       if (err) reject(err);
       resolve(token);
