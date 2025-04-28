10bf4129d19d2c4d31a3bc2c034f5061b5ff0619
Jhostym <you@example.com>
2025-04-28 16:38:41 -0500

    agredando dominios

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'https://administrativo.transportej.com',
+  'http://administrativo.transportej.com',
   'https://backendtjboletas.onrender.com'
 ];
 
